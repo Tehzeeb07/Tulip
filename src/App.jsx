3326d20c4c911d2b1ae64eb996145a98ff5a9e00
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-08 14:05:55 +0300

    setting up backend files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 
 function App() {
   const [count, setCount] = useState(0)
