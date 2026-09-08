f0ad7e31546988344b2ea84ce73a6eeb9914a90e
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-08 13:46:35 +0300

    setting up backend files

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,19 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ConvexReactClient } from "convex/react";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ConvexAuthProvider client={convex}>
+      <App />
+    </ConvexAuthProvider>
+  </React.StrictMode>,
+);
