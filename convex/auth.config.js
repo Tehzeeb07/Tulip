2c5c2f3d807c74d49c4cf59e0f2c22f934821ed0
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-12 14:44:12 +0300

    customs pagee data

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,14 +1,7 @@
-import { defineApp } from "convex/server";
-import resend from "@convex-dev/resend/convex.config";
-
-const app = defineApp();
-app.use(resend);
-export default app;
-
 export default {
   providers: [
     {
-      domain: globalThis.process?.env?.CONVEX_SITE_URL,
+      domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
   ],
