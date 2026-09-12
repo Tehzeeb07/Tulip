75b8a46f90a8a63a1dc93c0df4b4a24f24529669
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-12 12:59:33 +0300

    routes section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import Bouquets from "./pages/Bouquets";
+import Accessories from "./pages/Accessories";
 
 const VIDEO_URL =
   "https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260328_083109_283f3553-e28f-428b-a723-d639c617eb2b.mp4";
@@ -148,6 +149,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/bouquets" element={<Bouquets />} />
+        <Route path="/accessories" element={<Accessories />} />
       </Routes>
     </BrowserRouter>
   );
