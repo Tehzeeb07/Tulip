5ef2efa8c9ebdc7e9372a79eb7834227e26de00c
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-12 13:58:33 +0300

    occasions data

---
+++ src/pages/Occasions.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { OCCASIONS } from "../data/occasions";
+import "./Occasions.css";
+
+export default function Occasions() {
+  return (
+    <div className="occasions-page">
+      <div className="occasions-wrap">
+        <div className="occasions-header">
+          <h1>Occasions & Collections</h1>
+          <p>Curated arrangements for the moments that call for them.</p>
+        </div>
+
+        <div className="occasions-grid">
+          {OCCASIONS.map((o) => (
+            <Link to={`/occasions/${o.slug}`} className="occasion-tile" key={o.slug}>
+              <div className={`tile-img tile-${o.slug}`} />
+              <div className="tile-text">
+                <h2>{o.name}</h2>
+                <p>{o.tagline}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
