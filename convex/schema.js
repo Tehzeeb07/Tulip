37176f2c240af2f5175691e5ff641f9e51a8c3f7
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-14 13:47:38 +0300

    schema issue

--- convex/schema.js
+++ convex/schema.js
@@ -4,6 +4,16 @@ import { v } from "convex/values";
 
 export default defineSchema({
   ...authTables,
+  users: defineTable({
+    email: v.optional(v.string()),
+    emailVerificationTime: v.optional(v.float64()),
+    image: v.optional(v.string()),
+    isAnonymous: v.optional(v.boolean()),
+    name: v.optional(v.string()),
+    phone: v.optional(v.string()),
+    phoneVerificationTime: v.optional(v.float64()),
+    username: v.optional(v.string()),
+  }).index("email", ["email"]),
   favorites: defineTable({
     userId: v.id("users"),
     productId: v.string(),
