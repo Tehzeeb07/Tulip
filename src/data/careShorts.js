8b8bf4884185466afb9b3364b0576e5dfc10c41b
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-15 11:15:17 +0300

    bouquet care shots

---
+++ src/data/careShorts.js
@@ -0,0 +1,7 @@
+export const CARE_SHORTS = [
+  { id: 1, title: "Re-cutting stems", duration: "0:18", videoUrl: null },
+  { id: 2, title: "Changing the water", duration: "0:15", videoUrl: null },
+  { id: 3, title: "Removing wilted petals", duration: "0:12", videoUrl: null },
+  { id: 4, title: "Best vase placement", duration: "0:20", videoUrl: null },
+  { id: 5, title: "Reviving a drooping bouquet", duration: "0:25", videoUrl: null },
+];
