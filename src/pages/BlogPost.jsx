6e6fddc0d4c10b9d96995a1a3e6392275a081079
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-15 11:12:21 +0300

    blog post ui

---
+++ src/pages/BlogPost.jsx
@@ -0,0 +1,40 @@
+import { useParams, Link } from "react-router-dom";
+import { BLOG_POSTS } from "../data/blogPosts";
+import "./BlogPost.css";
+
+export default function BlogPost() {
+  const { slug } = useParams();
+  const post = BLOG_POSTS.find((p) => p.slug === slug);
+
+  if (!post) {
+    return (
+      <div className="blogpost-page">
+        <div className="blogpost-wrap">
+          <p className="not-found">
+            That article couldn't be found. <Link to="/blog">Back to the Journal</Link>
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="blogpost-page">
+      <div className="blogpost-wrap">
+        <Link to="/blog" className="back-link">← Back to the Journal</Link>
+
+        <span className="blogpost-category">{post.category}</span>
+        <h1>{post.title}</h1>
+        <span className="blogpost-readtime">{post.readTime}</span>
+
+        <div className="blogpost-img" />
+
+        <div className="blogpost-body">
+          {post.content.split("\n\n").map((para, i) => (
+            <p key={i}>{para}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
