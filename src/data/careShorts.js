df194aac6ee06df5701f21e359412e81e9e5cd65
Tehzeeb07 <tehzeeb.shkk@gmail.com>
2026-09-16 13:51:14 +0300

    img ui

--- src/data/careShorts.js
+++ src/data/careShorts.js
@@ -1,7 +1,7 @@
 export const CARE_SHORTS = [
-  { id: 1, title: "Re-cutting stems", duration: "0:20", videoUrl: "https://www.youtube.com/embed/zhb3HGPbfC0" },
-  { id: 2, title: "Changing the water", duration: "0:25", videoUrl: "https://www.youtube.com/embed/eoslMMmeaqw" },
-  { id: 3, title: "Removing wilted petals", duration: "0:22", videoUrl: "https://www.youtube.com/embed/OJEDm0ieSNY" },
-  { id: 4, title: "Best vase placement", duration: "0:21", videoUrl: "https://www.youtube.com/embed/E_KJroAO1RY" },
-  { id: 5, title: "Reviving a drooping bouquet", duration: "0:18", videoUrl: "https://www.youtube.com/embed/wksYukppG3k" },
+  { id: 1, title: "Re-cutting stems", duration: "0:18", videoUrl: "https://www.youtube.com/embed/zhb3HGPbfC0", image: null },
+  { id: 2, title: "Changing the water", duration: "0:15", videoUrl: "https://www.youtube.com/embed/eoslMMmeaqw", image: null },
+  { id: 3, title: "Removing wilted petals", duration: "0:12", videoUrl: "https://www.youtube.com/embed/OJEDm0ieSNY", image: null },
+  { id: 4, title: "Best vase placement", duration: "0:20", videoUrl: "https://www.youtube.com/embed/E_KJroAO1RY", image: null },
+  { id: 5, title: "Reviving a drooping bouquet", duration: "0:25", videoUrl: "https://www.youtube.com/embed/wksYukppG3k", image: null },
 ];
